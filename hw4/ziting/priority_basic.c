7478f9668cb60123ec23702a6ac1c51832e18e82
Ziting Shen <zitingshenbmc@gmail.com>
2018-03-18 23:11:21 -0400

    change priority

--- hw4/ziting/priority_basic.c
+++ hw4/ziting/priority_basic.c
@@ -18,9 +18,9 @@ int main(void) {
   if (thread_libinit(PRIORITY) == -1) exit(EXIT_FAILURE);
 
   char *hello_str = "Hello, world!";
-  int tid_1 = thread_create(hello_n1, hello_str, 0);
+  int tid_1 = thread_create(hello_n1, hello_str, -1);
   int tid_2 = thread_create(hello_0, hello_str, 0);
-  int tid_3 = thread_create(hello_1, hello_str, 0);
+  int tid_3 = thread_create(hello_1, hello_str, 1);
 
   printf("Simplest test case for PRIORITY.\n");
   printf("Print \"Hello, world! <priority>\" for 3 times on success.\n");
